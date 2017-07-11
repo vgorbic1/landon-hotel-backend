bf0a35343dd42628c851e3e6d12c1f82c74f4b7e
Vlad Gorbich <vladgorbich@gmail.com>
2017-07-11 18:47:48 -0500

    Implement a GET endpoint using Spring MVC

---
+++ src/main/java/com/gorbich/api/demofullstackappangularspringboot/model/Self.java
@@ -0,0 +1,14 @@
+package com.gorbich.api.demofullstackappangularspringboot.model;
+
+public class Self {
+	private String ref;
+
+	public String getRef() {
+		return ref;
+	}
+
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+	
+}
---
+++ src/main/java/com/gorbich/api/demofullstackappangularspringboot/rest/ResourceConstants.java
@@ -0,0 +1,5 @@
+package com.gorbich.api.demofullstackappangularspringboot.rest;
+
+public class ResourceConstants {
+	public static final String ROOM_RESERVATION_V1 = "/room/reservation/v1";
+}
