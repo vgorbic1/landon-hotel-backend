15a5c06be40eb044dfdcff2ca97e4ba638758def
Vlad Gorbich <vladgorbich@gmail.com>
2017-07-11 17:14:41 -0500

    initial commit

---
+++ src/main/java/com/gorbich/api/demofullstackappangularspringboot/DemofullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.gorbich.api.demofullstackappangularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofullStackAppAngularSpringBootApplication.class, args);
+	}
+}
