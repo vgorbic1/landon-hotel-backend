a8001d4c757b2c6f6fc0c1f5f87347036363a5de
Vlad Gorbich <vladgorbich@gmail.com>
2017-07-11 17:45:39 -0500

    add app configuration

--- src/main/java/com/gorbich/api/demofullstackappangularspringboot/DemofullStackAppAngularSpringBootApplication.java
+++ src/main/java/com/gorbich/api/demofullstackappangularspringboot/DemofullStackAppAngularSpringBootApplication.java
@@ -1,9 +1,13 @@
 package com.gorbich.api.demofullstackappangularspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class DemofullStackAppAngularSpringBootApplication {
 
 	public static void main(String[] args) {
